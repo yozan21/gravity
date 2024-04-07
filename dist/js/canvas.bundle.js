18a88ce024af2d896919f705bc6bb7171afeb4f0
yozan21 <yojankaphle80@gmail.com>
2024-04-07 09:35:13 +0545

    Initial commit

--- dist/js/canvas.bundle.js
+++ dist/js/canvas.bundle.js
@@ -97,81 +97,108 @@
 __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./utils */ "./src/js/utils.js");
 /* harmony import */ var _utils__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_utils__WEBPACK_IMPORTED_MODULE_0__);
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, _toPropertyKey(descriptor.key), descriptor); } }
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); Object.defineProperty(Constructor, "prototype", { writable: false }); return Constructor; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
-
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
-
-
-var canvas = document.querySelector('canvas');
-var c = canvas.getContext('2d');
+var canvas = document.querySelector("canvas");
+var c = canvas.getContext("2d");
 canvas.width = innerWidth;
 canvas.height = innerHeight;
 var mouse = {
   x: innerWidth / 2,
   y: innerHeight / 2
 };
-var colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']; // Event Listeners
+var colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
+var gravity = 1;
+var friction = 0.95;
 
-addEventListener('mousemove', function (event) {
+// Event Listeners
+addEventListener("mousemove", function (event) {
   mouse.x = event.clientX;
   mouse.y = event.clientY;
 });
-addEventListener('resize', function () {
-  canvas.width = innerWidth;
-  canvas.height = innerHeight;
+addEventListener("resize", function () {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
   init();
-}); // Objects
-
-var _Object = /*#__PURE__*/function () {
-  function Object(x, y, radius, color) {
-    _classCallCheck(this, Object);
-
+});
+addEventListener("click", function () {
+  init();
+});
+// Objects
+var Ball = /*#__PURE__*/function () {
+  function Ball(x, y, dx, dy, radius, color) {
+    _classCallCheck(this, Ball);
     this.x = x;
     this.y = y;
+    this.dx = dx;
+    this.dy = dy;
     this.radius = radius;
     this.color = color;
   }
-
-  _createClass(Object, [{
+  return _createClass(Ball, [{
     key: "draw",
     value: function draw() {
       c.beginPath();
       c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
       c.fillStyle = this.color;
       c.fill();
+      c.stroke();
       c.closePath();
     }
   }, {
     key: "update",
     value: function update() {
+      if (this.y + this.radius + this.dy > canvas.height) {
+        this.dy = -this.dy * friction;
+        Math.abs(this.dy) < 0.000001 ? this.dy = 0 : this.dy = this.dy;
+        // console.log(this.dy);
+      } else {
+        this.dy += gravity;
+      }
+      if (this.x + this.radius + this.dx > canvas.width || this.x - this.radius <= 0) {
+        if (this.dy == 0) {
+          this.dx = 0;
+        } else {
+          this.dx = -this.dx * friction;
+          Math.abs(this.dx) < 0.0001 ? this.dx = 0 : this.dx = this.dx;
+        }
+        // console.log(this.dy);
+      }
+      this.x += this.dx;
+      this.y += this.dy;
       this.draw();
     }
   }]);
-
-  return Object;
 }(); // Implementation
-
-
-var objects;
-
+var ballArr;
 function init() {
-  objects = [];
-
-  for (var i = 0; i < 400; i++) {// objects.push()
+  ballArr = [];
+  // ball = new Ball(canvas.width / 2, canvas.height / 2, 2, 30, "red");
+  for (var i = 0; i < 400; i++) {
+    // objects.push()
+    var x = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(0 + radius, canvas.width - radius);
+    var y = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(0, canvas.height - radius);
+    var dx = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(-2, 2);
+    var dy = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(-2, 2);
+    var radius = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomIntFromRange"])(8, 30);
+    var color = Object(_utils__WEBPACK_IMPORTED_MODULE_0__["randomColor"])(colors);
+    ballArr.push(new Ball(x, y, dx, dy, radius, color));
   }
-} // Animation Loop
+}
 
-
+// Animation Loop
 function animate() {
   requestAnimationFrame(animate);
   c.clearRect(0, 0, canvas.width, canvas.height);
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y); // objects.forEach(object => {
-  //  object.update()
-  // })
+  ballArr.forEach(function (ball) {
+    ball.update();
+  });
 }
-
 init();
 animate();
 
@@ -187,17 +214,14 @@ animate();
 function randomIntFromRange(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
 }
-
 function randomColor(colors) {
   return colors[Math.floor(Math.random() * colors.length)];
 }
-
 function distance(x1, y1, x2, y2) {
   var xDist = x2 - x1;
   var yDist = y2 - y1;
   return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
 }
-
 module.exports = {
   randomIntFromRange: randomIntFromRange,
   randomColor: randomColor,
--- src/js/canvas.js
+++ src/js/canvas.js
@@ -1,73 +1,107 @@
-import utils from './utils'
+import utils, { randomColor, randomIntFromRange } from "./utils";
 
-const canvas = document.querySelector('canvas')
-const c = canvas.getContext('2d')
+const canvas = document.querySelector("canvas");
+const c = canvas.getContext("2d");
 
-canvas.width = innerWidth
-canvas.height = innerHeight
+canvas.width = innerWidth;
+canvas.height = innerHeight;
 
 const mouse = {
   x: innerWidth / 2,
-  y: innerHeight / 2
-}
+  y: innerHeight / 2,
+};
 
-const colors = ['#2185C5', '#7ECEFD', '#FFF6E5', '#FF7F66']
+const colors = ["#2185C5", "#7ECEFD", "#FFF6E5", "#FF7F66"];
+
+const gravity = 1;
+const friction = 0.95;
 
 // Event Listeners
-addEventListener('mousemove', (event) => {
-  mouse.x = event.clientX
-  mouse.y = event.clientY
-})
+addEventListener("mousemove", (event) => {
+  mouse.x = event.clientX;
+  mouse.y = event.clientY;
+});
 
-addEventListener('resize', () => {
-  canvas.width = innerWidth
-  canvas.height = innerHeight
+addEventListener("resize", () => {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
 
-  init()
-})
-
+  init();
+});
+addEventListener("click", function () {
+  init();
+});
 // Objects
-class Object {
-  constructor(x, y, radius, color) {
-    this.x = x
-    this.y = y
-    this.radius = radius
-    this.color = color
+class Ball {
+  constructor(x, y, dx, dy, radius, color) {
+    this.x = x;
+    this.y = y;
+    this.dx = dx;
+    this.dy = dy;
+    this.radius = radius;
+    this.color = color;
   }
 
   draw() {
-    c.beginPath()
-    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
-    c.fillStyle = this.color
-    c.fill()
-    c.closePath()
+    c.beginPath();
+    c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+    c.fillStyle = this.color;
+    c.fill();
+    c.stroke();
+    c.closePath();
   }
 
   update() {
-    this.draw()
+    if (this.y + this.radius + this.dy > canvas.height) {
+      this.dy = -this.dy * friction;
+      Math.abs(this.dy) < 0.000001 ? (this.dy = 0) : (this.dy = this.dy);
+      // console.log(this.dy);
+    } else {
+      this.dy += gravity;
+    }
+    if (
+      this.x + this.radius + this.dx > canvas.width ||
+      this.x - this.radius <= 0
+    ) {
+      if (this.dy == 0) {
+        this.dx = 0;
+      } else {
+        this.dx = -this.dx * friction;
+        Math.abs(this.dx) < 0.0001 ? (this.dx = 0) : (this.dx = this.dx);
+      }
+      // console.log(this.dy);
+    }
+    this.x += this.dx;
+    this.y += this.dy;
+    this.draw();
   }
 }
 
 // Implementation
-let objects
+let ballArr;
 function init() {
-  objects = []
-
+  ballArr = [];
+  // ball = new Ball(canvas.width / 2, canvas.height / 2, 2, 30, "red");
   for (let i = 0; i < 400; i++) {
     // objects.push()
+    const x = randomIntFromRange(0 + radius, canvas.width - radius);
+    const y = randomIntFromRange(0, canvas.height - radius);
+    const dx = randomIntFromRange(-2, 2);
+    const dy = randomIntFromRange(-2, 2);
+    const radius = randomIntFromRange(8, 30);
+    const color = randomColor(colors);
+    ballArr.push(new Ball(x, y, dx, dy, radius, color));
   }
 }
 
 // Animation Loop
 function animate() {
-  requestAnimationFrame(animate)
-  c.clearRect(0, 0, canvas.width, canvas.height)
-
-  c.fillText('HTML CANVAS BOILERPLATE', mouse.x, mouse.y)
-  // objects.forEach(object => {
-  //  object.update()
-  // })
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  ballArr.forEach((ball) => {
+    ball.update();
+  });
 }
 
-init()
-animate()
+init();
+animate();
